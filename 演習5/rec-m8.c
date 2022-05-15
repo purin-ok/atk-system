f31058c33ace180cd159ee490915903ef12d7014
purin-ok <minute0816@gmail.com>
2022-05-15 22:38:43 +0900

    一応これで終わりと思う

--- 演習5/rec-m8.c
+++ 演習5/rec-m8.c
@@ -5,30 +5,46 @@
 
 #include "pi.h"
 
-#define A_BIAS 0x80
-#define F_S 11025
-#define H_LEN 4   /* チャンクサイズ */
-#define ID_LPCM 1 /* リニアPCMのID */
-typedef unsigned short uShort;
-typedef unsigned long uLong;
-char wave[] = "WAVE";
-char riff[] = "RIFF";
-char fmt[] = "fmt ";
-char a;
+//ヘッダファイル
+#define BIAS 0x80
+#define RIFF_SIZE 11061
+#define FMT_SIZE 16
+#define FORMAT_ID 1
+#define SAMPLING_RATE 11025
+#define CHANNEL_N 1
+#define BLOCK_SIZE 1
+#define Q_BIT 8
 
-void write_head(FILE *fp, uLong *header) {
-  fwrite("RIFF", sizeof(char), H_LEN, fp);
-  fwrite(header, sizeof(uLong), 4, fp);
-  fwrite("WAVE", sizeof(char), H_LEN, fp);
-  fwrite("fmt ", sizeof(char), H_LEN, fp);
+void header_data(FILE *fp) {
+  int riff_size = RIFF_SIZE;
+  int fmt_size = FMT_SIZE;
+  int format_id = FORMAT_ID;
+  int channel_n = CHANNEL_N;
+  int sampling_rate = SAMPLING_RATE;
+  int block_size = BLOCK_SIZE;
+  int q_bit = Q_BIT;
+
+  fwrite("RIFF", sizeof(char), 4, fp);
+  fwrite(&riff_size, sizeof(int), 1, fp);
+  fwrite("WAVE", sizeof(char), 4, fp);
+  fwrite("fmt ", sizeof(char), 4, fp);
+  fwrite(&fmt_size, sizeof(int), 1, fp);
+  fwrite(&format_id, sizeof(short), 1, fp);
+  fwrite(&channel_n, sizeof(short), 1, fp);
+  fwrite(&sampling_rate, sizeof(int), 1, fp);
+  fwrite(&sampling_rate, sizeof(int), 1, fp);
+  fwrite(&block_size, sizeof(short), 1, fp);
+  fwrite(&q_bit, sizeof(short), 1, fp);
+  fwrite("data", sizeof(char), 4, fp);
+  fwrite(&sampling_rate, sizeof(int), 1, fp);
 }
 
 int main(int argc, char **argv) {
   //ファイル名，振幅，振幅，書き込みファイル
   int t;
   double amp, frq, rad, vin;
-  int vout[F_S];
-  uLong header[32] = 0;
+  int vout[SAMPLING_RATE];
+
   FILE *fp;
   if (argc < 4) {
     printf("ちゃんと入力してね");
@@ -42,18 +58,17 @@ int main(int argc, char **argv) {
   amp = atof(argv[1]);
   frq = atof(argv[2]);
 
-  for (t = 0; t <= F_S; t++) {
+  for (t = 0; t <= SAMPLING_RATE; t++) {
     rad = t / (1000 / frq) * 2 * PI;
-    vin = amp * sin(rad) + A_BIAS; /* 標本化 */
+    vin = amp * sin(rad) + BIAS; /* 標本化 */
     if (vin < 0) vin = 0;
     if (vin > 255) vin = 255;
     vout[t] = vin; /* 量子化・符号化 */
     //    printf("%4d, %4d\n", t, vout[t]);  //ここをfwrite関数使って出力......?
   }
-  header[0] = F_S;
-  header[4] = "WAVE";
-  write_head(fp, header);
-  fwrite(vout, sizeof(int), F_S, fp);
+
+  header_data(fp);
+  fwrite(vout, sizeof(int), SAMPLING_RATE, fp);
   fclose(fp);
   return EXIT_SUCCESS;
 }
