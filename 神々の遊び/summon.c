51d07c516c4ff941fbd4d00c79a22e698af0bd9e
purin-ok <minute0816@gmail.com>
2022-08-29 17:56:47 +0900

    ええなこれ

--- 神々の遊び/summon.c
+++ 神々の遊び/summon.c
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-  char load[] = "12";
-  char path[] =
-      "\"cd CSV && hello.exe 12 && cd .. && hello.exe"
-      "\"";
-  system(path);
+  // char load[] = "12";
+  // char path[] =
+  //     "\"cd CSV && hello.exe 12 && cd .. && hello.exe"
+  //     "\"";
+  // system(path);
+  // puts("a\a\n");
+  // printf("%4d", 10000000);
   return 0;
 }
